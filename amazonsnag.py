80e14f055e3fd1160a650eda0d18ad63b728fc28
mattspond <mattspond12@tamu.edu>
2022-02-16 01:06:05 -0600

    commit on 'ponderbranch'

--- amazonsnag.py
+++ amazonsnag.py
@@ -1,1 +1,19 @@
+import re
+import datetime
 
+sixreq = 0
+totreq = 0
+sixdate = datetime.datetime(1995, 4, 11)
+
+with open("local_copy.log") as file_in:
+    for line in file_in:
+        logdate  = ""
+        datesearch = re.search('../.../....', line)
+        if datesearch is not None:
+            logdate = datesearch.group(0)
+            formlogdate = datetime.datetime.strptime(logdate,'%d/%b/%Y')
+            totreq = totreq + 1
+            if formlogdate > sixdate:
+                sixreq = sixreq +1
+print(totreq)
+print(sixreq)
