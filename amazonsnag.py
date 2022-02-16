67ca80269815fc8ea5e8adde817bfe62525c01a9
mattspond <64518201+mattspond@users.noreply.github.com>
2022-02-16 01:24:00 -0600

    Update amazonsnag.py

--- amazonsnag.py
+++ amazonsnag.py
@@ -1,5 +1,12 @@
 import re
 import datetime
+from urllib.request import urlretrieve
+
+URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+LOCAL_FILE = 'local_copy.log'
+
+local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
+
 
 sixreq = 0
 totreq = 0
